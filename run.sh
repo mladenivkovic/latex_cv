5c6299dfe7929088ca045a9cf8db28e60f215ff0
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2022-02-21 04:15:21 +0100

    moved setup to setup.tex

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-lualatex -synctex=1 -interaction=nonstopmode CV.tex
-lualatex -synctex=1 -interaction=nonstopmode CV.tex
+# lualatex -synctex=1 -interaction=nonstopmode CV.tex
+# lualatex -synctex=1 -interaction=nonstopmode CV.tex
+
+lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
+lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
