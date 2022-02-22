ec04dca67edb4b2de375770e26517c029180c0a3
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2022-02-22 01:32:43 +0100

    updated CV order

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# lualatex -synctex=1 -interaction=nonstopmode CV.tex
-# lualatex -synctex=1 -interaction=nonstopmode CV.tex
+lualatex -synctex=1 -interaction=nonstopmode CV.tex
+lualatex -synctex=1 -interaction=nonstopmode CV.tex
 
-lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
-lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
+# lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
+# lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
