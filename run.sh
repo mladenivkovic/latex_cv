749718119dc531110a37668a236f797ab455f578
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2022-02-22 04:11:32 +0100

    formatting hopefully done

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 lualatex -synctex=1 -interaction=nonstopmode CV.tex
-lualatex -synctex=1 -interaction=nonstopmode CV.tex
+# lualatex -synctex=1 -interaction=nonstopmode CV.tex
 
-# lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
-# lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
+lualatex -synctex=1 -interaction=nonstopmode example_cover_letter.tex
